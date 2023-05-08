392830bc05d7e519a2ae79c10ebccd2278544b5f
Shounak Sanjay Deshpande -T (shodeshp - ARICENT TECHNOLOGIES HOLDINGS LIMITED at Cisco) <shodeshp@cisco.com>
2023-05-09 02:14:17 +0530

    changed static file path

--- kbfc/kbfc/settings.py
+++ kbfc/kbfc/settings.py
@@ -120,12 +120,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "media/static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -134,10 +128,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/media/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media/static'), ) 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
